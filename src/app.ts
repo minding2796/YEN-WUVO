6deb8e247b9a23e02d097dcf6d619ca2a23105d7
minding2796 <minding2796@gmail.com>
2025-04-29 11:25:55 +0900

    Add HTML template, and enhance server routing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, {Express, Request, Response} from 'express';
+import path from 'path';
 
 const app: Express = express();
 app.set("port", process.env.PORT || 3000);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express Server!');
+    res.statusCode = 200;
+    res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
 
 app.listen(app.get("port"), () => {

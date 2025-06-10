ada677a3c8f82a1ec2741011f8a35b8979cd88b5
minding2796 <minding2796@gmail.com>
2025-06-10 12:04:41 +0900

    Implement stock simulation game logic and ranking system

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,42 @@
 import express, {Express, Request, Response} from 'express';
 import path from 'path';
+import {setInterval} from "node:timers";
+import {randomUUID} from "node:crypto";
 
 const app: Express = express();
+
+const stonk = {
+    value: 100000
+};
+
+const ranking = new Map();
+
 app.set("port", process.env.PORT || 3000);
+app.use(express.json());
+
+app.get('/uuid', (req: Request, res: Response) => {
+    res.statusCode = 200;
+    res.send(randomUUID());
+});
+
+app.get('/stonk', (req: Request, res: Response) => {
+    res.statusCode = 200;
+    res.send(JSON.stringify(stonk));
+});
+
+app.get('/ranking', (req: Request, res: Response) => {
+    res.statusCode = 200;
+    const list = [];
+    for (const item of ranking.values()) list.push(item);
+    res.send(JSON.stringify(list));
+});
+
+app.post('/submit', (req: Request, res: Response) => {
+    res.statusCode = 200;
+    const data = req.body;
+    ranking.set(data.uuid, data);
+    res.send('success');
+});
 
 app.get('/', (req: Request, res: Response) => {
     res.statusCode = 200;
@@ -11,4 +45,9 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(app.get("port"), () => {
     console.log(`Server is running at http://localhost:${app.get("port")}`);
+});
+
+setInterval(() => {
+    stonk.value += (Math.random() - 0.5) * 1000;
+    stonk.value = Math.round(Math.max(stonk.value, 0));
 });
